4a46bbc5abdbc32030cb8e2db216b8889b44c4a3
Tyuiu.NovikovAA@outlook.com <Tyuiu.NovikovAA@outlook.com>
2024-12-28 10:25:07 +0600

    Task0

--- Tyuiu.MerzhinskiyOO.Sprint5.Task0.V6.Lib/DataService.cs
+++ Tyuiu.MerzhinskiyOO.Sprint5.Task0.V6.Lib/DataService.cs
@@ -6,10 +6,19 @@ namespace Tyuiu.MerzhinskiyOO.Sprint5.Task0.V6.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
-            double z = x / (Math.Pow((Math.Pow(x, 2) + x), 0.5));
-            z = Math.Round(z, 3);
-            File.WriteAllText(path, Convert.ToString(z));
+            string path = "OutputFileTask0.txt";
+            double z;
+            try
+            {
+                z = x / (Math.Pow(Math.Pow(x, 2) + x, 0.5));
+                z = Math.Round(z, 3);
+                File.WriteAllText(path, Convert.ToString(z));
+                Console.WriteLine($"Файл успешно сохранён по пути: {path}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при записи в файл: {ex.Message}");
+            }
             return path;
         }
     }
