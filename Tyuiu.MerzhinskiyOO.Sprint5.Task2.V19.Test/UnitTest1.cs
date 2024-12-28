3647fb2ee3e382266651842fc4e206fa9bdaa4ba
Tyuiu.NovikovAA@outlook.com <Tyuiu.NovikovAA@outlook.com>
2024-12-28 10:51:51 +0600

    Task2

---
+++ Tyuiu.MerzhinskiyOO.Sprint5.Task2.V19.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Tyuiu.MerzhinskiyOO.Sprint5.Task2.V19.Lib;
+
+namespace Tyuiu.MerzhinskiyOO.Sprint5.Task2.V19.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.MerzhinskiyOO.Sprint5.Task2.V19/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.MerzhinskiyOO.Sprint5.Task2.V19.Lib;
+
+namespace Tyuiu.MerzhinskiyOO.Sprint5.Task2.V19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int[,] matrix = new int[3, 3] { { 9, 2, 5 },
+                                            { 8, 8, 2 },
+                                            { 7, 4, 8 } };
+            int rows = matrix.GetUpperBound(0) + 1;
+            int columns = matrix.Length / rows;
+            DataService ds = new DataService();
+
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.WriteLine($"{matrix[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string res = ds.SaveToFileTextData(matrix);
+            Console.WriteLine($"Файл: {res}");
+            Console.WriteLine("Успешно создан!");
+            Console.ReadKey();
+        }
+    }
+}
