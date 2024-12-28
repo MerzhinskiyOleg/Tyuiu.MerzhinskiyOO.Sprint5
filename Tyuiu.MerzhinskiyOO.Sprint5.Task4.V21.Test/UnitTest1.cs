65d06e67b65a202e494c68089db6815ad2190894
Tyuiu.NovikovAA@outlook.com <Tyuiu.NovikovAA@outlook.com>
2024-12-28 11:07:47 +0600

    Task4

---
+++ Tyuiu.MerzhinskiyOO.Sprint5.Task4.V21.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Tyuiu.MerzhinskiyOO.Sprint5.Task4.V21.Lib;
+
+namespace Tyuiu.MerzhinskiyOO.Sprint5.Task4.V21.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.MerzhinskiyOO.Sprint5.Task4.V21/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.MerzhinskiyOO.Sprint5.Task4.V21.Lib;
+
+namespace Tyuiu.MerzhinskiyOO.Sprint5.Task4.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
