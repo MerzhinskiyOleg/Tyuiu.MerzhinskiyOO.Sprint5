f8dbfacac473a312d771feb8ecdf3d213e2dda9c
Tyuiu.NovikovAA@outlook.com <Tyuiu.NovikovAA@outlook.com>
2024-12-28 10:59:24 +0600

    Task3

---
+++ Tyuiu.MerzhinskiyOO.Sprint5.Task3.V17.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Tyuiu.MerzhinskiyOO.Sprint5.Task3.V17.Lib;
+
+namespace Tyuiu.MerzhinskiyOO.Sprint5.Task3.V17.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.MerzhinskiyOO.Sprint5.Task3.V17/Program.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.MerzhinskiyOO.Sprint5.Task3.V17.Lib;
+
+namespace Tyuiu.MerzhinskiyOO.Sprint5.Task3.V17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            DataService ds = new DataService();
+            int x = 3;
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
