91e3bbf3b598d34a51ae7a611166bb80a1d6bcbb
Tyuiu.NovikovAA@outlook.com <Tyuiu.NovikovAA@outlook.com>
2024-12-28 10:36:02 +0600

    Task1

---
+++ Tyuiu.MerzhinskiyOO.Sprint5.Task1.V17/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.MerzhinskiyOO.Sprint5.Task1.V17.Lib;
+
+namespace Tyuiu.MerzhinskiyOO.Sprint5.Task1.V17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int startValue = 5;
+            int stopValue = -5;
+
+            DataService ds = new DataService();
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                             *");
+            Console.WriteLine("********************************************************************************");
+
+            Console.WriteLine("startValue = " + startValue);
+            Console.WriteLine("stopValue = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(startValue, stopValue);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
