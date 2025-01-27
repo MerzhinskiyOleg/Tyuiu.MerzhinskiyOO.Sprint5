49c37486433daf9cc105c7274251d6ca368f2877
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2025-01-27 19:31:12 +0500

    tasska

--- Tyuiu.MerzhinskiyOO.Sprint5.Task0.V6.Lib/DataService.cs
+++ Tyuiu.MerzhinskiyOO.Sprint5.Task0.V6.Lib/DataService.cs
@@ -6,20 +6,8 @@ namespace Tyuiu.MerzhinskiyOO.Sprint5.Task0.V6.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string path = "OutputFileTask0.txt";
-            double z;
-            try
-            {
-                z = x / (Math.Pow(Math.Pow(x, 2) + x, 0.5));
-                z = Math.Round(z, 3);
-                File.WriteAllText(path, Convert.ToString(z));
-                Console.WriteLine($"Файл успешно сохранён по пути: {path}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Ошибка при записи в файл: {ex.Message}");
-            }
-            return path;
+            double y = Math.Round((x)/(Math.Sqrt(Math.Pow(x,2)+ 2)),3);
+            return Convert.ToString(y);
         }
     }
 }
