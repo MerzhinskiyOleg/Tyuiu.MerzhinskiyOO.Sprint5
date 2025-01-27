85debc2e54cddcdf3cefb2665a4c44d65690afa6
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2025-01-27 19:31:46 +0500

    taskssas

--- Tyuiu.MerzhinskiyOO.Sprint5.Task0.V6.Lib/DataService.cs
+++ Tyuiu.MerzhinskiyOO.Sprint5.Task0.V6.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.MerzhinskiyOO.Sprint5.Task0.V6.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            double y = Math.Round((x)/(Math.Sqrt(Math.Pow(x,2)+ 2)),3);
+            double y = Math.Round((x)/(Math.Sqrt(Math.Pow(x,2)+ x)),3);
             return Convert.ToString(y);
         }
     }
